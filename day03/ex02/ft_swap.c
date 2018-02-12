4858a50a82a0e14c5344e1045127e49067cfcb98
Victor PHONGPHAYSANE <v.phongphaysane@gmail.com>
2018-02-13 00:42:52 +0100

    progress state

--- day03/ex02/ft_swap.c
+++ day03/ex02/ft_swap.c
@@ -1,55 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   compil.c                                           :+:      :+:    :+:   */
+/*   ft_swap.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vphongph <vphongph@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/11 19:55:25 by vphongph          #+#    #+#             */
-/*   Updated: 2018/02/12 21:39:27 by vphongph         ###   ########.fr       */
+/*   Updated: 2018/02/13 00:41:38 by vphongph         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	ft_print(unsigned int a)
-{
-	if (a >= 10)
-		ft_print(a / 10);
-	ft_putchar(a % 10 + '0');
-}
-
-void	ft_putnbr(int nb)
-{
-	unsigned int a;
-
-	a = (unsigned int)nb;
-	if (nb < 0)
-	{
-		ft_putchar('-');
-		a = (unsigned int)(-nb);
-	}
-	ft_print(a);
-}
-
-
-
-// int		main(void)
-// int	main(int ac, char **av)
-{
-	
-	
-	
-	// if (ac != 2)
-		// return 0;
-	// ft_putnbr(atoi(av[1]));	
-	// ft_putchar('\n');
-	return 0;
-}
