da84383ea100af75984b352e42f7bf22f1f45620
Victor PHONGPHAYSANE <v.phongphaysane@gmail.com>
2018-02-12 01:23:01 +0100

    push putnbr brouillon sa mere la pute ca marche pas

--- day02/ex06/ft_putnbr.c
+++ day02/ex06/ft_putnbr.c
@@ -1,12 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_.c                                              :+:      :+:    :+:   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vphongph <vphongph@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/11 19:55:25 by vphongph          #+#    #+#             */
-/*   Updated: 2018/02/11 22:31:27 by vphongph         ###   ########.fr       */
+/*   Updated: 2018/02/12 01:21:28 by vphongph         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void 	ft_putchar(char c);
+
+void	ft_print(unsigned int a)
+{
+	if (a >= 10)
+		ft_print(a / 10);
+	ft_putchar(a % 10 + '0');
+}
+
+void	ft_putnbr(int nb)
+{
+	unsigned int a;
+
+	if (nb < 0)
+		ft_putchar('-');
+	a = nb;
+	ft_print(a);
+}
