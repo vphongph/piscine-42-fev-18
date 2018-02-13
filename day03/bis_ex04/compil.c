8aaf141bda1e8ea26816e036dadec45e138dff04
Victor PHONGPHAYSANE <v.phongphaysane@gmail.com>
2018-02-13 16:51:20 +0100

    push ex04 day03 + bis

---
+++ day03/bis_ex04/compil.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   compil.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vphongph <vphongph@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/11 19:55:25 by vphongph          #+#    #+#             */
+/*   Updated: 2018/02/13 16:47:05 by vphongph         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print(unsigned int a)
+{
+	if (a >= 10)
+		ft_print(a / 10);
+	ft_putchar(a % 10 + '0');
+}
+
+void	ft_putnbr(int nb)
+{
+	unsigned int a;
+
+	a = (unsigned int)nb;
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		a = (unsigned int)(-nb);
+	}
+	ft_print(a);
+}
+
+void	ft_ultimate_div_mod(int *a, int *b);
+
+int		main(void)
+// int	main(int ac, char **av)
+{
+	int *lol;
+	int *mdr;
+	int nume;
+	int denom;
+
+	lol = &nume;
+	mdr = &denom;
+	nume = -2147483648;
+	denom = 2147483647;
+
+	printf("nume = %d\ndenom = %d\n", nume, denom);
+	ft_ultimate_div_mod(lol, mdr);	
+	printf("nume = %d\ndenom = %d\n", nume, denom);
+
+	// if (ac != 2)
+		// return 0;
+	// ft_putnbr(atoi(av[1]));	
+	// ft_putchar('\n');
+	return 0;
+}
