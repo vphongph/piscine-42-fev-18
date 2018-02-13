17312d425c191f20b87c470a4f6af2151b5414bd
Victor PHONGPHAYSANE <v.phongphaysane@gmail.com>
2018-02-13 20:47:21 +0100

    push ex06

---
+++ day03/ex06/compil.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   compil.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vphongph <vphongph@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/11 19:55:25 by vphongph          #+#    #+#             */
+/*   Updated: 2018/02/13 20:46:57 by vphongph         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print(unsigned int a)
+{
+	if (a >= 10)
+		ft_print(a / 10);
+	ft_putchar(a % 10 + '0');
+}
+
+void	ft_putnbr(int nb)
+{
+	unsigned int a;
+
+	a = (unsigned int)nb;
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		a = (unsigned int)(-nb);
+	}
+	ft_print(a);
+}
+
+int		ft_strlen(char *str);
+
+int		main(void)
+// int	main(int ac, char **av)
+{
+
+	char string[] = "Phongphaysane";
+
+	ft_putnbr(ft_strlen(string));
+	
+	// if (ac != 2)
+		// return 0;
+	// ft_putnbr(atoi(av[1]));	
+	// ft_putchar('\n');
+	return 0;
+}
