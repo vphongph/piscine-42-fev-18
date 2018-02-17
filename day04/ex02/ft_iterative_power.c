361898f3bb19e3af124f17912c2a9ae589d994c6
Victor PHONGPHAYSANE <v.phongphaysane@gmail.com>
2018-02-17 11:21:15 +0100

    push some ex from day09

---
+++ day04/ex02/ft_iterative_power.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vphongph <vphongph@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/11 19:55:25 by vphongph          #+#    #+#             */
+/*   Updated: 2018/02/16 16:11:44 by vphongph         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_power(int nb, int power)
+{
+	int i;
+
+	i = nb;
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	if (power == 1)
+		return (nb);
+	if (nb == 1)
+		return (1);
+	if (nb == 0 && power > 0)
+		return (0);
+	while (power > 1)
+	{
+		nb = nb * i;
+		power--;
+	}
+	return (nb);
+}
