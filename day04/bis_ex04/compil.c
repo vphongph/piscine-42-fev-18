93d881dfa38ac95298630011969f2a446ad5b568
Victor PHONGPHAYSANE <v.phongphaysane@gmail.com>
2018-02-19 12:46:58 +0100

    push day04 ex04

---
+++ day04/bis_ex04/compil.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   compil.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vphongph <vphongph@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/11 19:55:25 by vphongph          #+#    #+#             */
+/*   Updated: 2018/02/19 12:39:27 by vphongph         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print(unsigned int a)
+{
+	if (a >= 10)
+		ft_print(a / 10);
+	ft_putchar(a % 10 + '0');
+}
+
+void	ft_putnbr(int nb)
+{
+	unsigned int a;
+
+	a = (unsigned int)nb;
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		a = (unsigned int)(-nb);
+	}
+	ft_print(a);
+}
+
+//printf("blabla = \n%s", arg);
+
+int ft_fibonacci(int index);
+
+int		main(void)
+// int	main(int ac, char **av)
+{
+
+int index;	
+
+index = -2;
+
+while(index++ <= 48)
+printf("\nindex = %d\nfibo = %d\n",index, ft_fibonacci(index));
+
+	// if (ac != 2)
+		// return 0;
+	// ft_putnbr(atoi(av[1]));	
+	// ft_putchar('\n');
+	return 0;
+}
---
+++ day04/bis_ex04/ft_fibonacci.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vphongph <vphongph@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/11 19:55:25 by vphongph          #+#    #+#             */
+/*   Updated: 2018/02/19 12:42:04 by vphongph         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (index > 46)
+		return (-1);
+	if (index == 0)
+		return (0);
+	if (index == 1)
+		return (1);
+	if (index == 2)
+		return (1);
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
