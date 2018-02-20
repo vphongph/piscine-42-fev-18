1e2a9489e60c4b0124d00e3a2ce4b0f2a470e664
Victor PHONGPHAYSANE <v.phongphaysane@gmail.com>
2018-02-20 22:15:35 +0100

    test reseaux

---
+++ day05/ex03/compil.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   compil.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vphongph <vphongph@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/11 19:55:25 by vphongph          #+#    #+#             */
+/*   Updated: 2018/02/20 21:29:51 by vphongph         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print(unsigned int a)
+{
+	if (a >= 10)
+		ft_print(a / 10);
+	ft_putchar(a % 10 + '0');
+}
+
+void	ft_putnbr(int nb)
+{
+	unsigned int a;
+
+	a = (unsigned int)nb;
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		a = (unsigned int)(-nb);
+	}
+	ft_print(a);
+}
+
+//printf("blabla = \n%s", arg);
+
+
+
+int		main(void)
+// int	main(int ac, char **av)
+{
+	
+	char strsource[] = "123456";
+	char strdest[] = "321654545455454";
+
+	printf("\nsource = %s\ndest = %s\nstrcpy = %s\n", strsource, strdest, strcpy(strdest, strsource));
+	
+	// if (ac != 2)
+		// return 0;
+	// ft_putnbr(atoi(av[1]));	
+	// ft_putchar('\n');
+	return 0;
+}
